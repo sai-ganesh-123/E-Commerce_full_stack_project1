5d481cb836f8d9948c02fdd8b14863f70cb6bdc1
Sai Ganesh <saiganeshmutyala2005@gmail.com>
2025-07-23 20:13:13 +0530

    final commits

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -151,10 +151,6 @@ const onSubmitHandler = async(event)=>{
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe'?'bg-green-400':''}`}></p>
               <img src={assets.stripe_logo} className='h-5 mx-4' alt="" />
             </div>
-            <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay'?'bg-green-400':''}`}></p>
-              <img src={assets.razorpay_logo} className='h-5 mx-4' alt="" />
-            </div>
             <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod'?'bg-green-400':''}`}></p>
              <p className='text-gray-500 text-sm font-medium mx-4 '>CASH ON DELIVERY</p>
